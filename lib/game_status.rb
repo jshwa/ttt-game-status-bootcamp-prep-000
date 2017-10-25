9b7b9073272388ce6d3d6a489b3417e046d18588
Joshua Palacios <palacios.joshua@gmail.com>
2017-10-25 20:50:55 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,10 +21,8 @@ def won?(board)
     position_2 = board[win_combination[1]]
     position_3 = board[win_combination[2]]
     
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combination
-    else
-      "hello"
     end
   end
 end
